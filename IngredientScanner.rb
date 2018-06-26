4ea40a61651768966c911ce7a36672a6976fc924
Tam Lam <tamlam@Tams-MacBook.local>
2018-06-26 16:20:22 +1000

    Terminal app completed

---
+++ IngredientScanner.rb
@@ -0,0 +1,38 @@
+f= File.open("ingredient.txt",'r')
+#Ingredient to check for
+badSulfates = ["Alkylbenzene sulfonates","Alkyl Benzene Sulfonate","Ammonium laureth sulfate","Ammonium lauryl sulfate","Ammonium Xylenesulfonate",
+    "Sodium C14-16 Olefin Sulfonate","Sodium cocoyl sarcosinate","Sodium laureth sulfate","Sodium lauryl sulfate","Sodium lauryl sulfoacetate",
+    "Sodium myreth sulfate","Sodium Xylenesulfonate","TEA-dodecylbenzenesulfonate","Ethyl PEG-15 cocamine sulfate","Dioctyl sodium sulfosuccinate"]
+badSilicones = ["Dimethicone","Bis-aminopropyl dimethicone","Cetearyl methicone","Cetyl Dimethicone","Cyclopentasiloxane","Stearoxy Dimethicone",
+    "Stearyl Dimethicone","Trimethylsilylamodimethicone","Amodimethicone","Dimethicone","Dimethiconol","Behenoxy Dimethicone","Phenyl trimethicone"]
+badAlcohols = ["Denatured alcohol","SD alcohol 40","Witch hazel","Isopropanol","Ethanol","SD alcohol","Propanol","Propyl alcohol","Isopropyl alcohol"]
+otherBadIngredients = ["Mineral oil","Paraffinum liquidum","Petrolatum","Bees wax","candelilla wax"]
+
+foundIngredients = Array.new
+
+
+
+f.each_line do |line|
+    puts line
+    badSulfates.each do |ingredient|
+        regex = /#{ingredient}/i
+        if line.match(regex) then foundIngredients.push(ingredient)  end
+    end
+    badSilicones.each do |ingredient|
+        regex = /#{ingredient}/i
+        if line.match(regex) then foundIngredients.push(ingredient)  end
+    end
+    otherBadIngredients.each do |ingredient|
+        regex = /#{ingredient}/i
+        if line.match(regex) then foundIngredients.push(ingredient)  end
+    end
+    badAlcohols.each do |ingredient|
+        regex = /#{ingredient}/i
+        if line.match(regex) then foundIngredients.push(ingredient)  end
+    end
+end
+f.close()
+uniqFoundIngredients = foundIngredients.uniq()
+
+uniqFoundIngredients.each {|ingredient| puts "-----#{ingredient}"}
+
